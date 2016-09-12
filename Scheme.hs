3a82a9f891eec5b560398a4084da65e51cfc8fce
Andrej Tokarčík <andrejtokarcik@gmail.com>
2016-09-12 14:55:05 +0200

    Simple parser: LispVal's Atoms, Numbers, Strings and Bools.

--- Scheme.hs
+++ Scheme.hs
@@ -1,16 +1,46 @@
 module Main where
 
-import System.Environment
-import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad (liftM)
+import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec
 
-spaces :: Parser ()
-spaces = skipMany1 space
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
-symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first : rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+  where
+      symbol :: Parser Char
+      symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+         <|> parseString
+         <|> parseNumber
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
     Left  err -> "No match: " ++ show err
     Right val -> "Found value"
 
