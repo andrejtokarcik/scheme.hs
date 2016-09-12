7639456f98df9e6458c289e13a9a2a623229a1ea
Andrej Tokarčík <andrejtokarcik@gmail.com>
2016-09-12 13:31:57 +0200

    First Steps, Exercise 3.

--- Scheme.hs
+++ Scheme.hs
@@ -1,9 +1,16 @@
 module Main where
 import System.Environment
+import System.IO
  
 main :: IO ()
 main = do
+    {-
     args <- getArgs
     -- putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
     putStrLn $ args !! 0 ++ " + " ++ args !! 1 ++ " = " ++
       show ((read $ args !! 0) + (read $ args !! 1))
+    -}
+    putStr "Name: "
+    hFlush stdout
+    name <- getLine
+    putStrLn $ "Hello, " ++ name
