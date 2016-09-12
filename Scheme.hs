c96009fb1678197324deeec49ffc7f22d9d79d74
Andrej Tokarčík <andrejtokarcik@gmail.com>
2016-09-12 14:00:10 +0200

    Simple parser: LISP symbols only.

--- Scheme.hs
+++ Scheme.hs
@@ -1,16 +1,17 @@
 module Main where
+
 import System.Environment
-import System.IO
- 
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
 main :: IO ()
 main = do
-    {-
-    args <- getArgs
-    -- putStrLn ("Hello, " ++ args !! 0 ++ " " ++ args !! 1)
-    putStrLn $ args !! 0 ++ " + " ++ args !! 1 ++ " = " ++
-      show ((read $ args !! 0) + (read $ args !! 1))
-    -}
-    putStr "Name: "
-    hFlush stdout
-    name <- getLine
-    putStrLn $ "Hello, " ++ name
+         (expr:_) <- getArgs
+         putStrLn (readExpr expr)
