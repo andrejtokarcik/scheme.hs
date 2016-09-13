da1aa960d120b18693c0a22bb7bdee238a2a1255
Andrej Tokarčík <andrejtokarcik@gmail.com>
2016-09-13 14:24:34 +0200

    Evaluation, Part 1, Exercise 1.

--- Eval.hs
+++ Eval.hs
@@ -3,13 +3,16 @@ module Eval where
 import Internal
 
 primitives :: [(String, [LispVal] -> LispVal)]
-primitives = [("+", numericBinOp (+)),
-              ("-", numericBinOp (-)),
-              ("*", numericBinOp (*)),
-              ("/", numericBinOp div),
-              ("mod", numericBinOp mod),
-              ("quotient", numericBinOp quot),
-              ("remainder", numericBinOp rem)]
+primitives =  [("+", numericBinOp (+)),
+               ("-", numericBinOp (-)),
+               ("*", numericBinOp (*)),
+               ("/", numericBinOp div),
+               ("mod", numericBinOp mod),
+               ("quotient", numericBinOp quot),
+               ("remainder", numericBinOp rem),
+               ("string?", isString),
+               ("number?", isNumber),
+               ("symbol?", isSymbol)]
 
 numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
 numericBinOp op params = Number $ foldl1 op $ map unpackNum params
@@ -25,12 +28,28 @@ numericBinOp op params = Number $ foldl1 op $ map unpackNum params
     unpackNum _ = error "meh unpackNum _"
     -}
 
+isString :: [LispVal] -> LispVal
+isString [String _] = Bool True
+isString _          = Bool False
+
+isNumber :: [LispVal] -> LispVal
+isNumber [Number _] = Bool True
+isNumber _          = Bool False
+
+isSymbol :: [LispVal] -> LispVal
+isSymbol [Atom _]                 = Bool True
+isSymbol [List [Atom "quote", _]] = Bool True
+isSymbol _                        = Bool False
+
 apply :: String -> [LispVal] -> LispVal
 apply func args = maybe (error "meh apply") ($ args) $ lookup func primitives
 
 eval :: LispVal -> LispVal
+{-
 eval val@(String _) = val
 eval val@(Number _) = val
 eval val@(Bool   _) = val
+-}
 eval (List (Atom "quote" : [val])) = val
 eval (List (Atom func    : args))  = apply func $ map eval args
+eval val = val
