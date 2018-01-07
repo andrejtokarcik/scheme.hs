89bada4a5e97110d682a3942e86689e3335d78f4
Andrej Tokarčík <andrejtokarcik@gmail.com>
2018-01-07 23:46:51 +0100

    More primitives & refactor.

--- Eval.hs
+++ Eval.hs
@@ -3,29 +3,32 @@ module Eval where
 import Internal
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
-primitives =  [("+", numericBinOp (+)),
-               ("-", numericBinOp (-)),
-               ("*", numericBinOp (*)),
-               ("/", numericBinOp div),
-               ("mod", numericBinOp mod),
-               ("quotient", numericBinOp quot),
-               ("remainder", numericBinOp rem),
-               ("string?", totalUnOp isString),
+primitives =  [("string?", totalUnOp isString),
                ("number?", totalUnOp isNumber),
                ("symbol?", totalUnOp isSymbol),
                ("symbol->string", unOp symbolToString),
-               ("string->symbol", unOp stringToSymbol)]
-
-numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
-numericBinOp op params@[_, _] = mapM unpackNum params >>= return . Number . foldl1 op
-    where unpackNum :: LispVal -> ThrowsError Integer
-          unpackNum   (Number n)  = return n
-          unpackNum s@(String s') = unpackParsedNum $ reads s'
-                 where unpackParsedNum [(n, _)] = return n
-                       unpackParsedNum _        = throwError $ TypeMismatch "number" s
-          unpackNum (List [n])    = unpackNum n -- maybe don't require singletons?
-          unpackNum notNum        = throwError $ TypeMismatch "number" notNum
-numericBinOp _  params        = throwError $ NumArgs 2 params
+               ("string->symbol", unOp stringToSymbol),
+               ("+", numNumBinOp (+)),
+               ("-", numNumBinOp (-)),
+               ("*", numNumBinOp (*)),
+               ("/", numNumBinOp div),
+               ("mod", numNumBinOp mod),
+               ("quotient", numNumBinOp quot),
+               ("remainder", numNumBinOp rem),
+               ("=", numBoolBinOp (==)),
+               ("<", numBoolBinOp (<)),
+               (">", numBoolBinOp (>)),
+               ("/=", numBoolBinOp (/=)),
+               (">=", numBoolBinOp (>=)),
+               ("<=", numBoolBinOp (<=)),
+               ("&&", boolBoolBinOp (&&)),
+               ("||", boolBoolBinOp (||)),
+               ("string=?", strBoolBinOp (==)),
+               ("string<?", strBoolBinOp (<)),
+               ("string>?", strBoolBinOp (>)),
+               ("string<=?", strBoolBinOp (<=)),
+               ("string>=?", strBoolBinOp (>=))
+               ]
 
 unOp :: (LispVal -> ThrowsError LispVal) -> [LispVal] -> ThrowsError LispVal
 unOp op [param] = op param
@@ -48,11 +51,47 @@ isSymbol _        = Bool False
 
 symbolToString :: LispVal -> ThrowsError LispVal
 symbolToString (Atom a) = return $ String a
-symbolToString notAtom  = throwError $ TypeMismatch "atom" notAtom
+symbolToString notAtom  = throwError $ TypeMismatch "symbol" notAtom
 
 stringToSymbol :: LispVal -> ThrowsError LispVal
 stringToSymbol (String s) = return $ Atom s
 stringToSymbol notString  = throwError $ TypeMismatch "string" notString
+
+binOp :: (LispVal -> ThrowsError a) -> (b -> LispVal) -> (a -> a -> b) -> [LispVal] -> ThrowsError LispVal
+binOp unpack pack op [x, y] = do x' <- unpack x
+                                 y' <- unpack y
+                                 return . pack $ x' `op` y'
+binOp unpack pack _  params = throwError $ NumArgs 2 params
+
+binOpOnNums = binOp unpackNum
+    where unpackNum :: LispVal -> ThrowsError Integer
+          unpackNum (Number n)   = return n
+          unpackNum s@(String n) = case reads n of [(n', _)] -> return n'
+                                                   _         -> throwError $ TypeMismatch "number" s
+          unpackNum (List [n])   = unpackNum n
+          unpackNum notNum       = throwError $ TypeMismatch "number" notNum
+
+numNumBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
+numNumBinOp = binOpOnNums Number
+
+numBoolBinOp :: (Integer -> Integer -> Bool) -> [LispVal] -> ThrowsError LispVal
+numBoolBinOp = binOpOnNums Bool
+
+boolBoolBinOp :: (Bool -> Bool -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBoolBinOp = binOp unpackBool Bool
+    where unpackBool :: LispVal -> ThrowsError Bool
+          unpackBool (Bool b)   = return b
+          --unpackBool (List [b]) = unpackBool b
+          unpackBool notBool    = throwError $ TypeMismatch "boolean" notBool
+
+strBoolBinOp :: (String -> String -> Bool) -> [LispVal] -> ThrowsError LispVal
+strBoolBinOp = binOp unpackStr Bool
+    where unpackStr :: LispVal -> ThrowsError String
+          unpackStr (String s) = return s
+          unpackStr (Number s) = return $ show s
+          unpackStr (Bool s)   = return $ show s
+          --unpackStr (List [s]) = unpackStr s
+          unpackStr notStr     = throwError $ TypeMismatch "string" notStr
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function" func)
